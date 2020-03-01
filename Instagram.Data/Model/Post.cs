addc1176e53b7557a275adfbba933a98134c9d15
Olga Kovalchuk <kovalchuk21.04@gmail.com>
2020-03-01 20:32:16 +0300

    Base models: Post, Tag

---
+++ Instagram.Data/Model/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Instagram.Data.Model
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime Created{ get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+        public string Url { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
+
+        
+    }
+}
---
+++ Instagram.Data/Model/Tag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Instagram.Data.Model
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+    }
+}
