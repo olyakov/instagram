57f73e0214b6eab991047b7d2fab5b30a4a62f29
Olga Kovalchuk <kovalchuk21.04@gmail.com>
2020-03-01 20:30:35 +0300

    Initialize .net core 2.1 project

---
+++ Instagram.Data/InstagramDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Instagram.Data.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Instagram.Data
+{
+    public class InstagramDbContext : DbContext
+    {
+        public InstagramDbContext(DbContextOptions<InstagramDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
