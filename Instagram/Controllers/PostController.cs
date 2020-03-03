c210f277b76bb413cbba7d6d32b2f5010a298edc
Olga Kovalchuk <kovalchuk21.04@gmail.com>
2020-03-03 14:19:04 +0000

    Create Upload Post model

---
+++ Instagram/Controllers/PostController.cs
@@ -0,0 +1,20 @@
+﻿using Instagram.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Instagram.Controllers
+{
+    public class PostController : Controller
+    {
+        public IActionResult Upload()
+        {
+            var model = new UploadPostModel();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UploadNewPost()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Instagram/Models/UploadPostModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Instagram.Models
+{
+    public class UploadPostModel
+    {
+        public string Title { get; set; }
+        public string Tags { get; set; }
+        public IFormFile ImageUpload { get; set; }
+
+    }
+}
