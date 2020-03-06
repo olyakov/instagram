3a9195384f96b050d057dd29efce1f450757c9dd
Olga Kovalchuk <kovalchuk21.04@gmail.com>
2020-03-06 18:53:37 +0000

    Update Login page

---
+++ Instagram.Data/Model/Like.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Instagram.Data.Model
+{
+    public class Like
+    {
+        public string Id { get; set; }
+
+        public AspNetUsers Users { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+    }
+}
