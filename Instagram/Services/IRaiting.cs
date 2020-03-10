5af0c72b6cdda3837d91d25bea47356aef30c788
Olga Kovalchuk <kovalchuk21.04@gmail.com>
2020-03-10 12:12:17 +0000

    Add RaitingService

---
+++ Instagram/Services/IRaiting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Instagram.Data.Model;
+
+namespace Instagram.Services
+{
+    public interface IRaiting
+    {
+        Task SetLike(Post post, AspNetUsers user);
+    }
+}
---
+++ Instagram/Services/RaitingService.cs
@@ -0,0 +1,52 @@
+﻿using Instagram.Data;
+using Instagram.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Instagram.Services
+{
+    public class RaitingService : IRaiting
+    {
+        private readonly InstagramDbContext _ctx;
+
+        public RaitingService(InstagramDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+
+        public async Task SetLike(Post post, AspNetUsers user)
+        {
+            // var user = ActionWithUser.GetCurrentUserAsync(_httpContextAccessor, _userManager).Result;
+            // var post = GetById(postId);
+
+            Like like = new Like()
+            {
+                UserId = user.Id,
+                Post = post
+            };
+
+            bool truth = true;
+
+            truth = post.Likes != null && post.Likes.Any(l => l.UserId == user.Id);
+
+            if (truth)
+            {
+                var del_like = post.Likes.FirstOrDefault(l => l.UserId == user.Id);
+                _ctx.Likes.Remove(del_like);
+                _ctx.SaveChanges();
+            }
+            else
+            {
+                _ctx.Likes.Add(like);
+                await _ctx.SaveChangesAsync();
+                /* var updatePost = GetById(postId);
+                 if (updatePost.Likes == null)
+                     updatePost.Likes = new List<Like>();*/
+            }
+        }
+    }
+}
+
