c1294314379d61398a3401ba197692db8fe657c3
Olga Kovalchuk <kovalchuk21.04@gmail.com>
2020-03-12 17:01:57 +0000

    Create Follow Service

---
+++ Instagram/Services/FollowServise.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Instagram.Data;
+using Instagram.Data.Model;
+
+namespace Instagram.Services
+{
+    public class FollowServise : IFollow
+    {
+        private readonly InstagramDbContext _context;
+
+        public FollowServise(InstagramDbContext context)
+        {
+            _context = context;
+        }
+
+        public Follow GetFollow(string followerId, string followingId) =>
+            _context.Follows.FirstOrDefault(f => f.FollowerId == followerId &&
+                                                    f.FollowingId == followingId);
+
+        public IEnumerable<Follow> GetUserFollows(string userId) =>
+            _context.Follows.Where(f => f.FollowerId == userId);
+
+        public IEnumerable<Follow> GetAll() => _context.Follows;
+
+        public bool IsFollow(string followerId, string followingId) =>
+            _context.Follows.Any(f => f.FollowingId == followingId &&
+                                         f.FollowerId == followerId);
+
+        public void Add(Follow follow)
+        {
+            _context.Follows.Add(follow);
+            _context.SaveChanges();
+        }
+
+        public void Remove(Follow follow)
+        {
+            _context.Follows.Remove(follow);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Instagram/Services/IFollow.cs
@@ -0,0 +1,17 @@
+﻿using Instagram.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Instagram.Services
+{
+    public interface IFollow
+    {
+        Follow GetFollow(string followerId, string followingId);
+        IEnumerable<Follow> GetUserFollows(string userId);
+        IEnumerable<Follow> GetAll();
+        bool IsFollow(string followerId, string followingId);
+        void Add(Follow follow);
+        void Remove(Follow follow);
+    }
+}
