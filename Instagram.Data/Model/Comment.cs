d3c17216a47447cd521ad0efe55c6b6a773f74d3
Nastassia Skurat <nastyskyrat@gmail.com>
2020-03-17 15:36:52 +0000

    Create Comment Servise

--- Instagram.Data/Model/Comment.cs
+++ Instagram.Data/Model/Comment.cs
@@ -19,6 +19,9 @@ namespace Instagram.Data.Model
         public virtual Post Post { get; set; }
 
         [Required]
+        public int  PostId { get; set; }
+
+        [Required]
         [StringLength(1000)]
         public string Content { get; set; }
     }
---
+++ Instagram/Services/IComment.cs
@@ -0,0 +1,13 @@
+﻿using Instagram.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Instagram.Services
+{
+    public interface IComment
+    {
+        void AddComment(Comment comment);
+    }
+}
