c3d5e755b848934ab363a0646f77900b10668160
Olga Kovalchuk <kovalchuk21.04@gmail.com>
2020-03-19 12:06:29 +0000

    Add Post commentors view model

---
+++ Instagram/Models/PostCommentorsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Instagram.Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Instagram.Models
+{
+    public class PostCommentorsViewModel
+    {
+        public string UserId { get; set; }
+        public IEnumerable<Comment> Comments { get; set; }
+    }
+}
