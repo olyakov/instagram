a8bd6e5e6eee9b183396444726fa484f463650f8
Nastassia Skurat <nastyskyrat@gmail.com>
2020-03-25 12:39:41 +0000

    Create ReportList component

---
+++ Instagram/Components/ReportList.cs
@@ -0,0 +1,43 @@
+﻿using Instagram.Models;
+using Instagram.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Instagram.Components
+{
+    public class ReportList : ViewComponent
+    {
+        private readonly IPost _postService;
+        private readonly IUser _userService;
+        private readonly IFollow _followService;
+        private readonly IReport _reportService;
+
+        public ReportList(IPost postService,
+            IUser userService,
+            IFollow followService,
+            IReport reportService)
+        {
+            _postService = postService;
+            _userService = userService;
+            _followService = followService;
+            _reportService = reportService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string username)
+        {
+            var reports = _reportService.GetUserReport(username);
+            var user = _userService.GetUserByUsername(username);
+
+            var model = new AdminReportsViewModel()
+            {
+                Reports = reports,
+                User = user
+            };
+
+
+            return View("ReportList", model);
+        }
+    }
+}
