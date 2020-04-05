5cc7b559a32e245eba5c3860c67a33dc33545645
Olga Kovalchuk <kovalchuk21.04@gmail.com>
2020-04-05 09:16:51 +0000

    Add SignalR Server

---
+++ Instagram.SignalR.Server/ReportsPublisher.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Fiver.Asp.SignalR.Server
+{
+    public class ReportsPublisher : Hub
+    {
+        public Task PublishReport(string reportName)
+        {
+            return Clients.All.InvokeAsync("OnReportPublished", reportName);
+        }
+
+        public Task SetLike(string reportName)
+        {
+            return Clients.All.InvokeAsync("OnSetLike", reportName);
+        }
+
+        public Task SetLikeColor(string reportName)
+        {
+            return Clients.All.InvokeAsync("OnSetLikeColor", reportName);
+        }
+    }
+}
